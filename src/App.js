92a79794b7ec4e36750ae0b15a3d11f7effe83b1
Harsha D Hegde <harshahegde10s@gmail.com>
2024-11-30 18:00:02 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,30 @@
+
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import Menu from './components/Menu';
+import Reservations from './components/Reservations';
+import Contact from './components/Contact';
+import AboutUs from './components/About';
+import './App.css';
+
+const App = () => {
+  return (
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/menu" element={<Menu />} />
+          <Route path="/reservations" element={<Reservations />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/about" element={<AboutUs />} /> {/* Add About route */}
+        </Routes>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
+
